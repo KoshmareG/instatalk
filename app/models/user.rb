a0cc6c26a10bf4616d9848418d0a4ccf62feb710
KoshmareG <koshmareg@yandex.ru>
2023-01-31 23:45:21 +0700

    Realtime online users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,16 @@ class User < ApplicationRecord
 
   has_many :messages
 
+  after_update_commit :broadcast_to_online_users
+
   private
 
   def generate_nickname
     self.nickname = Faker::Name.first_name.downcase
+  end
+
+  def broadcast_to_online_users
+    broadcast_remove_to('online_users') unless status
+    broadcast_append_to('online_users', target: 'users-online') if status
   end
 end
