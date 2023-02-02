fc40bd6a77b815bc7689253c192ff7bcf79c4ab6
KoshmareG <koshmareg@yandex.ru>
2023-02-02 16:39:24 +0700

    Cleaning cookies after user logout

--- app/channels/online_channel.rb
+++ app/channels/online_channel.rb
@@ -1,12 +1,12 @@
 class OnlineChannel < Turbo::StreamsChannel
   def subscribed
-    $redis_users_online.hset('users', current_user.id, current_user.nickname)
+    $redis_users_online.hset('users', current_user&.id, current_user&.nickname)
     current_user&.broadcast_append_to('online_users', target: 'users-online')
     super
   end
 
   def unsubscribed
-    $redis_users_online.hdel('users', current_user.id)
+    $redis_users_online.hdel('users', current_user&.id)
     current_user&.broadcast_remove_to('online_users', target: "user_#{current_user.id}")
     super
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,3 +313,7 @@ end
 Warden::Manager.after_set_user do |user, auth, opts|
   auth.cookies.signed[:user_id] = user.id
 end
+
+Warden::Manager.before_logout do |user, auth, opts|
+  auth.cookies.delete :user_id
+end
